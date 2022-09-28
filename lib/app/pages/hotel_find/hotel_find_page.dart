370241519726b20d630712fbf21c9f2eec6f6b50
salsabilaamnes <amnesketty@gmail.com>
2022-09-28 19:21:23 +0700

    hotel_tile

--- lib/app/pages/hotel_find/hotel_find_page.dart
+++ lib/app/pages/hotel_find/hotel_find_page.dart
@@ -4,10 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
 import 'package:injector/injector.dart';
 
+import '../../widgets/hotel_tile.dart';
+
 class HotelFindPage extends View {
-  HotelFindPage({Key? key}) : super(key: key);
+  const HotelFindPage({Key? key}) : super(key: key);
   
   @override
+  // ignore: no_logic_in_create_state
   State<StatefulWidget> createState() {
     final hotelFindController = Injector.appInstance.get<HotelFindController>();
     return _HotelFindViewState(hotelFindController);
@@ -15,24 +18,28 @@ class HotelFindPage extends View {
 }
 
 class _HotelFindViewState extends ViewState<HotelFindPage, HotelFindController> {
-  _HotelFindViewState(super._controller);
+  _HotelFindViewState(super.controller);
 
   @override
   Widget get view => Scaffold(
-    key: globalKey,
+    //key: globalKey,
     appBar: AppBar(
+      backgroundColor: const Color(0XFFE67E22),
       title: const Text('List of Booked Hotels'),
     ),
+    backgroundColor: const Color(0XFFD3D3D3),
     body: ControlledWidgetBuilder<HotelFindController>(
       builder: (BuildContext _, HotelFindController controller) =>
         controller.isLoading
         ? const Center(child: CupertinoActivityIndicator())
         : ListView.builder(
-          itemCount: controller.hotels.length,
+          //shrinkWrap: true,
+          itemCount: controller.hotels.length,            
           itemBuilder: (BuildContext _, int index) {
-            final hotels = controller.hotels[index];
-            final rooms = hotels.roomsHotel.length.toString();
-            return Text(rooms);
+            final hotel = controller.hotels[index];
+            final room = hotel.roomsHotel.first.price.toString();
+            // return Text(rooms);
+            return HotelTile(hotel: hotel);
           }
         )),
   );
