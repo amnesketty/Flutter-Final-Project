bc8556460538714546c7301797541b494c891a79
salsabilaamnes <amnesketty@gmail.com>
2022-10-06 17:33:06 +0700

    addFacilities

--- lib/app/pages/hotel_detail/hotel_detail_page.dart
+++ lib/app/pages/hotel_detail/hotel_detail_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/cupertino.dart';
 import 'package:lounga/app/pages/hotel_detail/hotel_detail_controller.dart';
 import 'package:flutter/material.dart';
@@ -70,167 +72,133 @@ class _HotelDetailViewState
               )),
           Row(children: [
             SizedBox(width: MediaQuery.of(context).size.width * 0.02),
-            Container(
-                margin: const EdgeInsets.only(left: 8.0, right: 8.0, top: 8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text('HOTELS',
-                        // ignore: prefer_const_constructors
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.w900)),
-                    const SizedBox(height: 7),
-                    Row(
-                      children: [
-                        const Icon(Icons.location_on,
-                            color: Colors.grey, size: 18),
-                        SizedBox(
-                            width: MediaQuery.of(context).size.width * 0.01),
-                        SizedBox(
-                          width: MediaQuery.of(context).size.width * 0.88,
-                          child: Text(widget.hotel.address,
-                              // ignore: prefer_const_constructors
-                              style: TextStyle(
-                                  fontSize: 15, fontWeight: FontWeight.w500)),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 7),
-                    Text(widget.hotel.phone,
-                        // ignore: prefer_const_constructors
-                        style: TextStyle(
-                            fontSize: 15, fontWeight: FontWeight.w500)),
-                    const SizedBox(height: 7),
-                    const Text('FACILITIES',
-                        // ignore: prefer_const_constructors
-                        style: TextStyle(
-                            fontSize: 15, fontWeight: FontWeight.w900)),
-                    const SizedBox(height: 7),
-                    Column(
-                      // if (widget.hotel.facilitiesHotel.airConditioner == true) {
-                      children: [
-                        Text(
-                            widget.hotel.facilitiesHotel.airConditioner.toString()),
-                         
-                          Icon(Icons.wind_power_sharp),
-                        // }
+            Expanded(
+              child: Container(
+                  margin: const EdgeInsets.only(left: 8.0, right: 8.0, top: 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text('HOTELS',
+                          // ignore: prefer_const_constructors
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w900)),
+                      const SizedBox(height: 7),
+                      Row(
+                        children: [
+                          const Icon(Icons.location_on,
+                              color: Colors.grey, size: 18),
+                          SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.01),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.88,
+                            child: Text(widget.hotel.address,
+                                // ignore: prefer_const_constructors
+                                style: TextStyle(
+                                    fontSize: 15, fontWeight: FontWeight.w500)),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 7),
+                      Text(widget.hotel.phone,
+                          // ignore: prefer_const_constructors
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w500)),
+                      const SizedBox(height: 7),
+                      const Text('FACILITIES',
+                          // ignore: prefer_const_constructors
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w900)),
+                      const SizedBox(height: 7),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        // mainAxisSize: MainAxisSize.max,
                         
-                      ],
-                    ),
-                    const Text('ROOMS',
-                        // ignore: prefer_const_constructors
-                        style: TextStyle(
-                            fontSize: 15, fontWeight: FontWeight.w900)),
-                    const SizedBox(height: 7),
-                    Container(
-                        height: MediaQuery.of(context).size.height * 0.238,
-                        width: MediaQuery.of(context).size.width * 0.92,
-                        // ignore: prefer_const_constructors
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.grey),
-                            borderRadius: BorderRadius.circular(10)),
-                        child: ControlledWidgetBuilder<HotelDetailController>(
-                          builder: (BuildContext context, HotelDetailController controller) {
-                            var typeRoom = Set<String>();
-                            List<RoomsHotel> uniqueTypeRoomList = widget.hotel.roomsHotel
-                              .where((element) => element.bookingHotels.isEmpty)
-                              .where((room) => typeRoom.add(room.type))
-                              .toList();
-                            //print(typeRoom.length);
-                            //uniqueTypeRoomList.forEach((element) {print(element.type);});
-                            //print(uniqueTypeRoomList.forEach((element) {print(element);}));
-                            return ListView.builder(
-                            itemCount: typeRoom.length,
-                            itemBuilder: (BuildContext context, int index)
-                            => Container(
-                            margin: const EdgeInsets.all(7),
-                            child: 
-                            Row(
-                              // ignore: prefer_const_literals_to_create_immutables
-                              children: [
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  // ignore: prefer_const_literals_to_create_immutables
-                                  children: [
-                                    Text(uniqueTypeRoomList[index].type,
-                                        style: TextStyle(
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.w800)),
-                                    Text(
-                                    'Rp ${uniqueTypeRoomList[index].price}/room/night',
-                                    // textAlign: TextAlign.right,
-                                    style: const TextStyle(
-                                        fontSize: 13,
-                                        fontWeight: FontWeight.w800,
-                                        color: Color(0XFFE67E22))),
-                                  ],
-                                ),
-                                const Spacer(),
-                                TextButton(
-                                    onPressed: () {
-                                      //var seen = Set<String>();
-                                      //List<RoomsHotel> uniquelist = widget.hotel.roomsHotel.where((room) => seen.add(room.type)).toList();
-                                      //print(uniquelist);
-                                      controller.navigateToHotelBooking(
-                                        widget.hotel, widget.user, widget.bookingDate,
-                                        widget.totalRoom, uniqueTypeRoomList[index]);
-                                    },
-                                    style: TextButton.styleFrom(
-                                        backgroundColor: const Color(0XFFE67E22)),
-                                    child: const Text('Choose',
-                                        style:
-                                            TextStyle(color: Colors.white)))
-                              ],
-                            ),
-                          )
-                          ,);
-                          }
-                            ),
-                        ),
-                        
-                    //     child: Container(
-                    //       margin: const EdgeInsets.all(7),
-                    //       child: ControlledWidgetBuilder<HotelDetailController>(
-                    //         builder: (BuildContext context, HotelDetailController controller) =>
-                    //       Row(
-                    //         // ignore: prefer_const_literals_to_create_immutables
-                    //         children: [
-                    //           Column(
-                    //             crossAxisAlignment: CrossAxisAlignment.start,
-                    //             // ignore: prefer_const_literals_to_create_immutables
-                    //             children: [
-                    //               Text(widget.hotel.roomsHotel.first.type,
-                    //                   style: TextStyle(
-                    //                       fontSize: 20,
-                    //                       fontWeight: FontWeight.w800)),
-                    //               Text(
-                    //               'Rp ${widget.hotel.roomsHotel.first.price}/room/night',
-                    //               // textAlign: TextAlign.right,
-                    //               style: const TextStyle(
-                    //                   fontSize: 13,
-                    //                   fontWeight: FontWeight.w800,
-                    //                   color: Color(0XFFE67E22))),
-                    //             ],
-                    //           ),
-                    //           const Spacer(),
-                    //           TextButton(
-                    //               onPressed: () {
-                    //                 controller.navigateToHotelBooking(
-                    //                   widget.hotel, widget.user, widget.bookingDate,
-                    //                   widget.totalRoom, widget.hotel.roomsHotel.first.price, widget.hotel.roomsHotel.first.id);
-                    //               },
-                    //               style: TextButton.styleFrom(
-                    //                   backgroundColor: const Color(0XFFE67E22)),
-                    //               child: const Text('Choose',
-                    //                   style:
-                    //                       TextStyle(color: Colors.white)))
-                    //         ],
-                    //       ),
-                    //     )
-                    //     ,)
-                    
-                  ],
-                ))
+                        children: [
+                          Icon(Icons.air_rounded, color: widget.hotel.facilitiesHotel.airConditioner == true ? Color(0XFFE67E22) : Colors.grey),
+                          Icon(Icons.connected_tv_rounded, color: widget.hotel.facilitiesHotel.television == true ? Color(0XFFE67E22) : Colors.grey),
+                          Icon(Icons.wifi, color: widget.hotel.facilitiesHotel.wifi == true ? Color(0XFFE67E22) : Colors.grey),
+                          Icon(Icons.food_bank_rounded, color: widget.hotel.facilitiesHotel.restaurant == true ? Color(0XFFE67E22) : Colors.grey),
+                          Icon(Icons.spa, color: widget.hotel.facilitiesHotel.spa == true ? Color(0XFFE67E22) : Colors.grey),
+                          Icon(Icons.pool, color: widget.hotel.facilitiesHotel.pool == true ? Color(0XFFE67E22) : Colors.grey),
+                          Icon(Icons.child_care_rounded, color: widget.hotel.facilitiesHotel.playground == true ? Color(0XFFE67E22) : Colors.grey),
+                          Icon(Icons.sports_gymnastics, color: widget.hotel.facilitiesHotel.gym == true ? Color(0XFFE67E22) : Colors.grey),
+                          Icon(Icons.local_parking_rounded, color: widget.hotel.facilitiesHotel.parking == true ? Color(0XFFE67E22) : Colors.grey),
+                        ],
+                      ),
+                      const Text('ROOMS',
+                          // ignore: prefer_const_constructors
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w900)),
+                      const SizedBox(height: 7),
+                      Container(
+                          height: MediaQuery.of(context).size.height * 0.238,
+                          width: MediaQuery.of(context).size.width * 0.92,
+                          // ignore: prefer_const_constructors
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.grey),
+                              borderRadius: BorderRadius.circular(10)),
+                          child: ControlledWidgetBuilder<HotelDetailController>(
+                            builder: (BuildContext context, HotelDetailController controller) {
+                              var typeRoom = Set<String>();
+                              List<RoomsHotel> uniqueTypeRoomList = widget.hotel.roomsHotel
+                                .where((element) => element.bookingHotels.isEmpty)
+                                .where((room) => typeRoom.add(room.type))
+                                .toList();
+                              //print(typeRoom.length);
+                              //uniqueTypeRoomList.forEach((element) {print(element.type);});
+                              //print(uniqueTypeRoomList.forEach((element) {print(element);}));
+                              return ListView.builder(
+                              itemCount: typeRoom.length,
+                              itemBuilder: (BuildContext context, int index)
+                              => Container(
+                              margin: const EdgeInsets.all(7),
+                              child: 
+                              Row(
+                                // ignore: prefer_const_literals_to_create_immutables
+                                children: [
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    // ignore: prefer_const_literals_to_create_immutables
+                                    children: [
+                                      Text(uniqueTypeRoomList[index].type,
+                                          // ignore: prefer_const_constructors
+                                          style: TextStyle(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.w800)),
+                                      Text(
+                                      'Rp ${uniqueTypeRoomList[index].price}/room/night',
+                                      // textAlign: TextAlign.right,
+                                      style: const TextStyle(
+                                          fontSize: 13,
+                                          fontWeight: FontWeight.w800,
+                                          color: Color(0XFFE67E22))),
+                                    ],
+                                  ),
+                                  const Spacer(),
+                                  TextButton(
+                                      onPressed: () {
+                                        //var seen = Set<String>();
+                                        //List<RoomsHotel> uniquelist = widget.hotel.roomsHotel.where((room) => seen.add(room.type)).toList();
+                                        //print(uniquelist);
+                                        controller.navigateToHotelBooking(
+                                          widget.hotel, widget.user, widget.bookingDate,
+                                          widget.totalRoom, uniqueTypeRoomList[index]);
+                                      },
+                                      style: TextButton.styleFrom(
+                                          backgroundColor: const Color(0XFFE67E22)),
+                                      child: const Text('Choose',
+                                          style:
+                                              TextStyle(color: Colors.white)))
+                                ],
+                              ),
+                            )
+                            ,);
+                            }
+                              ),
+                          ),             
+                    ],
+                  )),
+            )
           ]),
         ],
       ));
